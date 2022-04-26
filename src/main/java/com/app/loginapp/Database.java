4fd8df85fa0ce343d0c7cb525fc1a52c7320685d
Jakub Nitkiewicz <01158207@pw.edu.pl>
2022-04-26 10:58:48 +0200

    Changed Database class methods so that they use procedures saved in database, added methods for changing username and password

--- src/main/java/com/app/loginapp/Database.java
+++ src/main/java/com/app/loginapp/Database.java
@@ -3,11 +3,12 @@ package com.app.loginapp;
 import java.sql.*;
 
 public class Database {
+
     static boolean checkIfUserExists(String username, String password) {
         try {
             String connectionString = "jdbc:sqlserver://plan-task-server.database.windows.net:1433;database=planTask;user=JakubNitkiewicz;password=planTask123;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
             Connection con = DriverManager.getConnection(connectionString);
-            PreparedStatement statement = con.prepareStatement("SELECT username, password FROM loginInfo WHERE username= ? AND password= ? ");
+            PreparedStatement statement = con.prepareStatement("EXEC GetUser @username = ?, @password = ?");
             statement.setString(1, username);
             statement.setString(2, password);
             ResultSet resultSet = statement.executeQuery();
@@ -17,11 +18,12 @@ public class Database {
         }
         return false;
     }
+
     static boolean checkIfUsernameExists(String username) {
         try {
             String connectionString = "jdbc:sqlserver://plan-task-server.database.windows.net:1433;database=planTask;user=JakubNitkiewicz;password=planTask123;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
             Connection con = DriverManager.getConnection(connectionString);
-            PreparedStatement statement = con.prepareStatement("SELECT username FROM loginInfo WHERE username= ?");
+            PreparedStatement statement = con.prepareStatement("EXEC GetUsername @username = ?");
             statement.setString(1, username);
             ResultSet resultSet = statement.executeQuery();
             return resultSet.next();
@@ -35,9 +37,37 @@ public class Database {
         try {
             String connectionString = "jdbc:sqlserver://plan-task-server.database.windows.net:1433;database=planTask;user=JakubNitkiewicz;password=planTask123;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
             Connection con = DriverManager.getConnection(connectionString);
-            PreparedStatement statement = con.prepareStatement("INSERT INTO loginInfo(username, password) VALUES (?, ?)");
+            PreparedStatement statement = con.prepareStatement("EXEC AddUser @username = ?, @password = ?");
             statement.setString(1, username);
             statement.setString(2, password);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void ChangePassword(String username, String oldPassword, String newPassword) throws SQLException {
+        try {
+            String connectionString = "jdbc:sqlserver://plan-task-server.database.windows.net:1433;database=planTask;user=JakubNitkiewicz;password=planTask123;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            Connection con = DriverManager.getConnection(connectionString);
+            PreparedStatement statement = con.prepareStatement("EXEC ChangePassword @username = ?, @oldPassword = ?, @newPassword = ?");
+            statement.setString(1, username);
+            statement.setString(2, oldPassword);
+            statement.setString(2, newPassword);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void ChangeUsername(String username, String password, String newUsername) throws SQLException {
+        try {
+            String connectionString = "jdbc:sqlserver://plan-task-server.database.windows.net:1433;database=planTask;user=JakubNitkiewicz;password=planTask123;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            Connection con = DriverManager.getConnection(connectionString);
+            PreparedStatement statement = con.prepareStatement("EXEC ChangeUsername @username = ?, @password = ?, @newUsername = ?");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            statement.setString(2, newUsername);
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
